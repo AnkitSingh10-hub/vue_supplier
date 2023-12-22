8033fb90a6b22c394befd03f6735e6409b2c7ef8
ankit <ankitsinghtheweeknd@gmail.com>
2023-12-22 10:15:15 +0545

    pagination3

---
+++ src/views/Pages/ActivityDetail.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <h1>Item Detail</h1>
+        <h1>it's ID is {{ route.params.id }}</h1>
+
+        <div class="details">
+            <p class="detail-label">Item Name:</p>
+            <p id="itemName">Item Name Placeholder</p>
+
+            <p class="detail-label">Description:</p>
+            <p id="itemDescription">Item Description Placeholder</p>
+        </div>
+    </div>
+    <pre>
+        {{ activityStore.activityDetailResponse }}
+        {{
+            $route.params.id }}</pre>
+</template>
+
+<script setup lang="ts">
+import { defineComponent, onMounted } from "vue"
+import { useRoute } from "vue-router"
+import { useActivityStore } from '@/stores/activity'
+const activityStore = useActivityStore()
+const route = useRoute()
+onMounted(() => {
+    activityStore.getApiActivityDetail(route.params.id)
+})
+
+
+</script>
+
+<style lang="scss" scoped></style>
